f9935809395df9b5d239dc01071dc5f52fe33fda
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-06-26 21:54:46 -0500

    new addition of some utility functions

--- support/utils.js
+++ support/utils.js
@@ -18,4 +18,24 @@ function placeShips(gameboard){
 }
 
 
+function placeShip(gameboard, ship){
+    let { i, j } = generateRandomCordinates();
+    let axis = Math.random() < .5 ? 'x': 'y';
+    
+
+    while(!GameBoard.canPlaceShip(ship, [i,j], axis)){
+        ({ i,j }) = generateRandomCordinates();
+        axis = Math.random() < .5 ? 'x': 'y';
+    }
+
+    gameboard.placeShip(ship, [i, j], axis);
+}
+
+function generateRandomCordinates(){
+    return {
+        i: Math.floor(Math.random() * (9 - 0 + 1) + 0),
+        j: Math.floor(Math.random() * (9 - 0 + 1) + 0),
+      };
+}
+
 
